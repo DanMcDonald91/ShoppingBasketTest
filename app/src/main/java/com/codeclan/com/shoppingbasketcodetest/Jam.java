840621a8d70307ae46f53c3ac46f5865205ce38c
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-03-19 19:37:49 +0000

    added jam class, inherits name and price from item

--- app/src/main/java/com/codeclan/com/shoppingbasketcodetest/Jam.java
+++ app/src/main/java/com/codeclan/com/shoppingbasketcodetest/Jam.java
@@ -4,5 +4,8 @@ package com.codeclan.com.shoppingbasketcodetest;
  * Created by user on 17/03/2017.
  */
 
-public class Jam {
+public class Jam extends Item {
+    public Jam(String name, int price) {
+        super(name, price);
+    }
 }
