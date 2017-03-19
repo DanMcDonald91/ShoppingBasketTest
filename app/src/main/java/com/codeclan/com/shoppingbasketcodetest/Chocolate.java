aa2bff78b534095698f9ba5585ffb9bccceb1c52
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-03-19 19:36:22 +0000

    Chocolate class inherits name and price from item parent class

--- app/src/main/java/com/codeclan/com/shoppingbasketcodetest/Chocolate.java
+++ app/src/main/java/com/codeclan/com/shoppingbasketcodetest/Chocolate.java
@@ -4,5 +4,8 @@ package com.codeclan.com.shoppingbasketcodetest;
  * Created by user on 17/03/2017.
  */
 
-public class Chocolate {
+public class Chocolate extends Item {
+    public Chocolate(String name, int price) {
+        super(name, price);
+    }
 }
