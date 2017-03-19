959c2ab0a2adb70e9feadc1cf39a816f38712871
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-03-19 19:29:36 +0000

    Created item class and added getters and setters

---
+++ app/src/main/java/com/codeclan/com/shoppingbasketcodetest/Bread.java
@@ -0,0 +1,8 @@
+package com.codeclan.com.shoppingbasketcodetest;
+
+/**
+ * Created by user on 17/03/2017.
+ */
+
+public class Bread extends Item {
+}
---
+++ app/src/main/java/com/codeclan/com/shoppingbasketcodetest/Item.java
@@ -0,0 +1,41 @@
+package com.codeclan.com.shoppingbasketcodetest;
+
+/**
+ * Created by user on 17/03/2017.
+ */
+
+public abstract class Item {
+
+    // The item class has two fields
+
+    public String name;
+    public int price;
+
+
+    // The item class has one constructor
+    // Constructors are invoked to create objects from the class blueprint.
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // The item class has four methods
+
+    public String getName() {
+        return name;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void SetPrice(int price) {
+        this.price = price;
+    }
+}
