7cd31efd42bd91aea8c7b39be84dde2c6101a675
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-03-19 19:40:12 +0000

    created shopping basket class, created item arraylist in the constructor

--- app/src/main/java/com/codeclan/com/shoppingbasketcodetest/ShoppingBasket.java
+++ app/src/main/java/com/codeclan/com/shoppingbasketcodetest/ShoppingBasket.java
@@ -1,8 +1,17 @@
 package com.codeclan.com.shoppingbasketcodetest;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 17/03/2017.
  */
 
 public class ShoppingBasket {
+
+    protected ArrayList<Item> items;
+
+    public ShoppingBasket() {
+        this.items = new ArrayList<Item>();
+    }
 }
+g
